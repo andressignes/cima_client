7ae92c824f8016f368aeef7301e4f390d77b61a4
Andrés Signes <asignes86@gmail.com>
2021-12-29 23:47:53 +0100

    Home design

--- lib/src/home/views/home_page.dart
+++ lib/src/home/views/home_page.dart
@@ -1,8 +1,10 @@
+import 'dart:developer';
+import 'dart:io';
+
 import 'package:cima_client/src/core/widgets/drawer_widget.dart';
-import 'package:cima_client/src/problemas_suministro/views/problemas_suministro_page.dart';
-import 'package:cima_client/src/search/views/serch_page.dart';
-import 'package:cima_client/src/settings/settings_view.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -14,29 +16,49 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('⚕️ CIMA Client'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.report_problem),
-            onPressed: () {
-              Navigator.restorablePushNamed(
-                  context, ProblemasSuministroPage.routeName);
-            },
-          ),
-          IconButton(
-            icon: const Icon(Icons.search),
-            onPressed: () {
-              Navigator.restorablePushNamed(context, SearchPage.routeName);
-            },
-          ),
-          IconButton(
-            icon: const Icon(Icons.settings),
-            onPressed: () {
-              Navigator.restorablePushNamed(context, SettingsView.routeName);
-            },
-          ),
-        ],
       ),
-      body: const Center(child: Text('Home')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Text(
+              'Bienvenido al Centro de Información de Medicamentos de la AEMPS',
+              style: Theme.of(context).textTheme.headline5,
+            ),
+            Text(
+              'Aqui podras consultar los medicamentos que tienen disponibles en el inventario, ver los ultimos medicamentos autorizados y los medicamentos que tienen algun tipo de problemas de suministro.',
+              style: Theme.of(context).textTheme.bodyText2,
+            ),
+            SizedBox(height: 16),
+            Text(
+              'Para mas informacion dirigete a la web de la AEMPS o ponte en contacto en alguna de sus redes sociales.',
+              style: Theme.of(context).textTheme.bodyText2,
+            ),
+            const Spacer(),
+            SizedBox(
+              height: 50,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  TextButton(
+                    onPressed: () => _openLink('https://www.aemps.gob.es/'),
+                    child: Image.asset('assets/images/web.png'),
+                  ),
+                  TextButton(
+                    onPressed: () => _openLink('https://twitter.com/AEMPSGOB'),
+                    child: Image.asset('assets/images/twitter.png'),
+                  ),
+                  TextButton(
+                    onPressed: () =>
+                        _openLink('https://www.youtube.com/AempsGobEsinfo'),
+                    child: Image.asset('assets/images/youtube.png'),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
       drawer: const DrawerWidget(),
       // floatingActionButton: FloatingActionButton(
       //   onPressed: () {
@@ -45,5 +67,35 @@ class HomePage extends StatelessWidget {
       //   child: const Icon(Icons.search),
       // ),
     );
+  }
+
+  void _openLink(String url) async {
+    if (kIsWeb) {
+      await launch(
+        url,
+        forceSafariVC: false,
+      );
+      return;
+    }
+    if (await canLaunch(url)) {
+      if (Platform.isAndroid) {
+        await launch(url);
+      } else if (Platform.isIOS) {
+        if (!await launch(
+          url,
+          forceSafariVC: false,
+          universalLinksOnly: true,
+        )) {
+          await launch(url, forceSafariVC: true);
+        }
+      } else {
+        await launch(
+          url,
+          forceSafariVC: false,
+        );
+      }
+    } else {
+      log('Could not launch $url');
+    }
   }
 }
