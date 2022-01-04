9106a4ab8cd026bdf90e5b967f98591dc719b006
Andrés Signes <asignes86@gmail.com>
2022-01-04 09:31:23 +0100

    rename file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:cima_api/cima_api.dart';
-import 'package:cima_client/src/core/env/envirorment_config.dart';
+import 'package:cima_client/src/core/env/environment_config.dart';
 import 'package:cima_repository/cima_repository.dart';
 import 'package:flutter/material.dart';
 
