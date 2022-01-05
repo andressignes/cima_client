12f800d4613504b04d4d44f593ec4be46a137fe0
Andrés Signes <asignes86@gmail.com>
2022-01-05 16:02:17 +0100

    test atc

--- packages/cima_model/test/src/atc_test.dart
+++ packages/cima_model/test/src/atc_test.dart
@@ -3,6 +3,22 @@ import 'package:test/test.dart';
 
 void main() {
   group('Atc', () {
+    final atcJson = {
+      "codigo": "N02B",
+      "nombre": "OTROS ANALGÉSICOS Y ANTIPIRÉTICOS",
+      "nivel": 3
+    };
+
+    final atcObj = Atc(
+      nombre: 'OTROS ANALGÉSICOS Y ANTIPIRÉTICOS',
+      codigo: 'N02B',
+      nivel: 3,
+    );
+
+    test('can be instantiated', () {
+      expect(atcObj, isNotNull);
+    });
+
     test('supports value comparisons', () {
       expect(
           Atc(codigo: '', nivel: 0, nombre: '') ==
@@ -10,11 +26,12 @@ void main() {
           isTrue);
     });
 
-    test('supports value comparisons', () {
-      expect(
-          Atc(codigo: '', nivel: 1, nombre: '') ==
-              Atc(codigo: '', nivel: 0, nombre: ''),
-          isFalse);
+    test('json serialization fromJson', () {
+      expect(Atc.fromJson(atcJson), atcObj);
+    });
+
+    test('json serialization toJson', () {
+      expect(atcObj.toJson(), atcJson);
     });
   });
 }
