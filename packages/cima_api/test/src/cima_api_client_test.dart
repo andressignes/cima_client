2e1f5a3480f608ff60140e71068f8d0bdcc89780
Andrés Signes <asignes86@gmail.com>
2022-01-18 17:59:54 +0100

    testing api

--- packages/cima_api/test/src/cima_api_client_test.dart
+++ packages/cima_api/test/src/cima_api_client_test.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:cima_api/cima_api.dart';
 import 'package:http/http.dart';
@@ -10,13 +11,19 @@ class MockHttpClient extends Mock implements Client {}
 void main() {
   group('CimaApiClient', () {
     late final Client client;
-    final String baseUrl = 'cima.aemps.es';
+
+    const String baseUrl = 'cima.aemps.es';
+    final jsonResponse =
+        File('test/data/medicamento_cn_712729.json').readAsStringSync();
     setUpAll(() {
       client = MockHttpClient();
     });
 
-    test('can be instantiated', () {
+    test('can be instantiated without client', () {
       expect(CimaApiClient(baseUrl: baseUrl), isNotNull);
+    });
+
+    test('can be instantiated with client', () {
       expect(
           CimaApiClient(
             httpClient: MockHttpClient(),
@@ -26,144 +33,11 @@ void main() {
     });
 
     group('getMedicamento', () {
-      test('get medication by CN', () async {
+      test('get medication with valid cn', () async {
         final cn = '712729';
         final params = <String, String>{};
         params['cn'] = cn;
-        final jsonResponse = {
-          "nregistro": "51347",
-          "nombre": "ASPIRINA C 400 mg/240 mg COMPRIMIDOS EFERVESCENTES",
-          "pactivos": "ACETILSALICILICO ACIDO, ASCORBICO ACIDO",
-          "labtitular": "Bayer Hispania, S.L.",
-          "cpresc": "Sin Receta",
-          "estado": {"aut": 107737200000},
-          "comerc": true,
-          "receta": false,
-          "generico": false,
-          "conduc": false,
-          "triangulo": false,
-          "huerfano": false,
-          "biosimilar": false,
-          "nosustituible": {"id": 0, "nombre": "N/A"},
-          "psum": false,
-          "notas": false,
-          "materialesInf": false,
-          "ema": false,
-          "docs": [
-            {
-              "tipo": 1,
-              "url": "https://cima.aemps.es/cima/pdfs/ft/51347/FT_51347.pdf",
-              "urlHtml":
-                  "https://cima.aemps.es/cima/dochtml/ft/51347/FT_51347.html",
-              "secc": true,
-              "fecha": 1628287030000
-            },
-            {
-              "tipo": 2,
-              "url": "https://cima.aemps.es/cima/pdfs/p/51347/P_51347.pdf",
-              "urlHtml":
-                  "https://cima.aemps.es/cima/dochtml/p/51347/P_51347.html",
-              "secc": true,
-              "fecha": 1628287041000
-            }
-          ],
-          "fotos": [
-            {
-              "tipo": "materialas",
-              "url":
-                  "https://cima.aemps.es/cima/fotos/thumbnails/materialas/51347/51347_materialas.jpg",
-              "fecha": 1519196603000
-            },
-            {
-              "tipo": "formafarmac",
-              "url":
-                  "https://cima.aemps.es/cima/fotos/thumbnails/formafarmac/51347/51347_formafarmac.jpg",
-              "fecha": 1519196602000
-            }
-          ],
-          "atcs": [
-            {
-              "codigo": "N02B",
-              "nombre": "OTROS ANALGÉSICOS Y ANTIPIRÉTICOS",
-              "nivel": 3
-            },
-            {
-              "codigo": "N02BA",
-              "nombre": "Ácido salicílico y derivados",
-              "nivel": 4
-            },
-            {
-              "codigo": "N02BA51",
-              "nombre":
-                  "Ácido acetilsalicílico, combinaciones excluyendo psicolépticos",
-              "nivel": 5
-            }
-          ],
-          "principiosActivos": [
-            {
-              "id": 176,
-              "codigo": "176A",
-              "nombre": "ACETILSALICILICO ACIDO",
-              "cantidad": "400",
-              "unidad": "mg",
-              "orden": 1
-            },
-            {
-              "id": 61,
-              "codigo": "61A",
-              "nombre": "ASCORBICO ACIDO",
-              "cantidad": "240",
-              "unidad": "mg",
-              "orden": 2
-            }
-          ],
-          "excipientes": [
-            {
-              "id": 475,
-              "nombre": "HIDROGENO CARBONATO DE SODIO",
-              "cantidad": "900",
-              "unidad": "mg",
-              "orden": 1
-            },
-            {
-              "id": 2100,
-              "nombre": "CARBONATO DE SODIO ANHIDRO",
-              "cantidad": "200",
-              "unidad": "mg",
-              "orden": 2
-            },
-            {
-              "id": 2362,
-              "nombre": "CITRATO DE SODIO (E-331)",
-              "cantidad": "1242.0",
-              "unidad": "mg",
-              "orden": 9
-            }
-          ],
-          "viasAdministracion": [
-            {"id": 48, "nombre": "VÍA ORAL"}
-          ],
-          "presentaciones": [
-            {
-              "cn": "712729",
-              "nombre":
-                  "ASPIRINA C 400 mg/240 mg COMPRIMIDOS EFERVESCENTES , 10 comprimidos",
-              "estado": {"aut": 107737200000},
-              "comerc": true,
-              "psum": false
-            }
-          ],
-          "formaFarmaceutica": {"id": 43, "nombre": "COMPRIMIDO EFERVESCENTE"},
-          "formaFarmaceuticaSimplificada": {
-            "id": 13,
-            "nombre": "COMPRIMIDO EFERVESCENTE"
-          },
-          "vtm": {
-            "id": 139011000140103,
-            "nombre": "ácido acetilsalicílico + ácido ascórbico"
-          },
-          "dosis": "400/240 mg/mg"
-        };
+
         when(
           () => client.get(Uri.https(
             baseUrl,
@@ -180,6 +54,27 @@ void main() {
         expect(result.statusCode, 200);
       });
 
+      test('get medication with invalid cn', () async {
+        final cn = 'fake';
+        final params = <String, String>{};
+        params['cn'] = cn;
+
+        when(
+          () => client.get(Uri.https(
+            baseUrl,
+            '/cima/rest/medicamento',
+            params,
+          )),
+        ).thenAnswer((_) async => Response('', 204));
+        final apiClient = CimaApiClient(
+          httpClient: client,
+          baseUrl: baseUrl,
+        );
+        final result = await apiClient.getMedicationByCN(cn);
+        expect(result, isA<Response>());
+        expect(result.statusCode, 204);
+      });
+
       test('get medication by nregistro', () async {
         final nRegistro = '51347';
         final params = <String, String>{};
@@ -190,7 +85,7 @@ void main() {
             '/cima/rest/medicamento',
             params,
           )),
-        ).thenAnswer((_) async => Response('', 200));
+        ).thenAnswer((_) async => Response(jsonResponse, 200));
         final apiClient = CimaApiClient(
           httpClient: client,
           baseUrl: baseUrl,
