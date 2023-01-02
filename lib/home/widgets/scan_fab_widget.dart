e2639e2bc205297f037226a050da9e194ca5164d
Andrés Signes <asignes86@gmail.com>
2023-01-02 22:39:35 +0100

    feat: scan code

--- lib/home/widgets/scan_fab_widget.dart
+++ lib/home/widgets/scan_fab_widget.dart
@@ -1,13 +1,19 @@
 import 'dart:developer';
 
+import 'package:cima_client/app/router/routes.dart';
 import 'package:cima_client/l10n/l10n.dart';
-import 'package:cima_client/medication_detail/medication_detail.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:go_router/go_router.dart';
 
-class ScanFabWidget extends StatelessWidget {
+class ScanFabWidget extends StatefulWidget {
   const ScanFabWidget({super.key});
 
+  @override
+  State<ScanFabWidget> createState() => _ScanFabWidgetState();
+}
+
+class _ScanFabWidgetState extends State<ScanFabWidget> {
   @override
   Widget build(BuildContext context) {
     final l10n = context.l10n;
@@ -22,19 +28,18 @@ class ScanFabWidget extends StatelessWidget {
           false,
           ScanMode.DEFAULT,
         );
+        if (!mounted) return;
         if (barcodeScanRes.isEmpty || barcodeScanRes.length < 12) {
           return;
         }
         log('Barcode: $barcodeScanRes');
         final nationalCode = barcodeScanRes.substring(6, 12);
         log('cn: $nationalCode');
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => MedicationDetailPage(
-              cn: nationalCode,
-            ),
-          ),
+        context.pushNamed(
+          Routes.medicationDetail.name,
+          queryParams: {
+            'cn': nationalCode,
+          },
         );
       },
       child: const Icon(Icons.qr_code_scanner),
